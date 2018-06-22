6d82246c79a3b228f10fc157e7cc2e0076d8e0b9
SurajSonawane <suraj2sonawane@gmail.com>
2018-06-22 10:49:23 +0530

    f5fyt

--- SCOOPEN/src/com/scp/DemoSeriliazation/Account.java
+++ SCOOPEN/src/com/scp/DemoSeriliazation/Account.java
@@ -8,10 +8,28 @@ public class Account implements Serializable {
 String name= "suraj";
 transient String password="JAI MAHARASHTRA";
 
+
+@Override
+public String toString() {
+	return "Account [name=" + name + ", password=" + password + "]";
+}
+
+public Account() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public Account(String name, String password) {
+	super();
+	this.name = name;
+	this.password = password;
+}
+
 private void writeObject(ObjectOutputStream os)throws Exception{
 	 //os.writeObject(password);
-	password+="123";
+	
 	os.writeObject(name);
+	password="123"+password;
 	os.writeObject(password);
 	os.defaultWriteObject();
 }
@@ -19,6 +37,8 @@ private void writeObject(ObjectOutputStream os)throws Exception{
 private void readObject(ObjectInputStream in)throws Exception{
 	name=(String) in.readObject();
 	password=  (String) in.readObject();
+	password=password.substring(3);
+	// password=password.spz
 	//password=epass.substring(3);
 }
 }
